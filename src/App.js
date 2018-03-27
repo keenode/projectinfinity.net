472df55a4c22163bad6e8d8dd66f8a0a4984c026
keenode <kstaffieri@gmail.com>
2018-03-26 22:04:44 -0700

    adding logic for initial test routes, home and play

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
+
+import Home from './containers/Home/Home'
+import Play from './containers/Play/Play'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <nav>
+            <Link to="/">Home</Link><br />
+            <Link to="/play">Play</Link>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/play" component={Play} />
+          </nav>
+        </Router>
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/containers/Play/Play.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const play = () => (
+  <div>
+    <h1>Play</h1>
+  </div>
+)
+
+export default play
