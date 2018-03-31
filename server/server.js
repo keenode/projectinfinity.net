7ec88512cf22fc58bcd8a1260ba145f0d2d6de2c
keenode <kstaffieri@gmail.com>
2018-03-31 15:28:37 -0700

    setting up and trying out server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const SERVER_PORT = process.env.PORT || 9001
+const CLIENT_ORIGIN = 'http://localhost:3000'
+
+// CORS Middleware
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api', (req, res) => res.send({ message: 'Hello from PI API!' }))
+
+app.listen(SERVER_PORT, () => console.log('PI Server listening on port ' + SERVER_PORT))
--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home'
 import Play from './containers/Play/Play'
 
 class App extends Component {
-  render() {
+  state = {
+    message: ''
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:9001/api')
+      .then(res => {
+        console.log(res)
+        this.setState({ message: res.data.message })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render () {
     return (
       <Layout>
+        <p>Message from API: {this.state.message}</p>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/play" component={Play} />
