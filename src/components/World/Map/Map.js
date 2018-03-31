33bc2e54a331442ac583cd23c577f26506870a52
keenode <kstaffieri@gmail.com>
2018-03-31 12:46:24 -0700

    playing with map sizing

--- src/components/World/Map/Map.js
+++ src/components/World/Map/Map.js
@@ -6,7 +6,14 @@ import styles from './Map.css'
 class Map extends Component {
   componentDidMount () {
     console.log('[Map] Did Mount')
-    const app = new PIXI.Application()
+    const app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      // antialias: true
+    })
+    app.renderer.backgroundColor = 0x061639
+    // app.renderer.autoResize = true
+    // app.renderer.resize(window.innerWidth, window.innerHeight)
     document.getElementById('world').appendChild(app.view)
   }
 
