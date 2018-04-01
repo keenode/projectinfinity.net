08d195a143f040e1493efecc2de2b36e9601477d
keenode <kstaffieri@gmail.com>
2018-03-31 20:13:29 -0700

    adding first test CRUD endpoints to api

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
-// const mongo = require('mongodb')
-// const assert = require('assert')
-const routes = require('./routes/api')
 
-const url = 'mongodb://localhost:27017/projectinfinity'
+mongoose.connect('mongodb://localhost/projectinfinity')
+mongoose.Promise = global.Promise
+
+const routes = require('./routes/api')
 
 const SERVER_PORT = process.env.PORT || 9001
 const CLIENT_ORIGIN = 'http://localhost:9000'
@@ -16,9 +18,17 @@ app.use(function (req, res, next) {
   next()
 })
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => res.send('PI Server API'))
 app.get('/api', (req, res) => res.send({ message: 'Hello from PI API!' }))
 
 app.use('/api', routes)
+
+// Error handling middleware
+app.use(function (err, req, res, next) {
+  // console.log(err)
+  res.status(422).send({ error: err.message })
+})
 
 app.listen(SERVER_PORT, () => console.log('PI Server listening on port ' + SERVER_PORT))
