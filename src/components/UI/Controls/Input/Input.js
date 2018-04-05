7df60df9ba449e162281e4d089231aacead87280
keenode <kstaffieri@gmail.com>
2018-04-05 14:23:34 -0700

    working on data binding to login form

--- src/components/UI/Controls/Input/Input.js
+++ src/components/UI/Controls/Input/Input.js
@@ -5,15 +5,24 @@ import styles from './Input.css'
 const input = (props) => {
   let inputElement = null
 
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case ('input'):
-      inputElement = <input {...props} />
+      inputElement = <input {...props.elementConfig} value={props.value} onChange={props.changed} />
       break
     case ('textarea'):
-      inputElement = <textarea {...props} />
+      inputElement = <textarea {...props.elementConfig} onChange={props.changed} />
+      break
+    case ('select'):
+      inputElement = (
+        <select value={props.value} onChange={props.changed}>
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>
+      )
       break
     default:
-      inputElement = <input {...props} />
+      inputElement = <input {...props.elementConfig} value={props.value} onChange={props.changed} />
   }
 
   return (
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,14 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Input from '../../components/UI/Controls/Input/Input'
 
 import styles from './Login.css'
 
-const login = () => (
-  <section className={styles.Login}>
-    <h1>Login</h1>
-    <Input inputtype="input" type="text" name="username" placeholder="Username" />
-  </section>
-)
+class Login extends Component {
+  state = {
+    loginForm: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          nmame: 'email',
+          placeholder: 'Email'
+        },
+        value: ''
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          name: 'password',
+          placeholder: 'Password'
+        },
+        value: ''
+      },
+      rememberMe: {
+        elementType: 'input',
+        elementConfig: {
+          checked: false
+        },
+        value: false
+      }
+    }
+  }
 
-export default login
+  inputChangedHandler = (event, inputId) => {
+    const updatedLoginForm = {
+      ...this.state.loginForm
+    }
+    const updatedFormElement = {
+      ...updatedLoginForm[inputId]
+    }
+    updatedFormElement.value = event.target.value
+    updatedLoginForm[inputId] = updatedFormElement
+    this.setState({ loginForm: updatedLoginForm })
+  }
+
+  render() {
+    const formElementsArray = []
+    for (let key in this.state.loginForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.loginForm[key]
+      })
+    }
+    return (
+      <section className={styles.Login}>
+        <h1>Login</h1>
+        <hr />
+        {formElementsArray.map(formElement => (
+          <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType} 
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => { this.inputChangedHandler(event, formElement.id) }} />
+        ))}
+      </section>
+    )
+  }
+}
+
+export default Login
