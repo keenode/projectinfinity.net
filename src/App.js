027686dcc6fe7de9e8ba88668acab5183c1590cc
keenode <kstaffieri@gmail.com>
2018-04-05 13:12:15 -0700

    adding files for new Login route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home'
 import Play from './containers/Play/Play'
+import Login from './containers/Login/Login'
 
 class App extends Component {
   render () {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/play" component={Play} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Layout>
     )
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Login.css'
+
+const login = () => (
+  <section className={styles.Login}>
+    <h1>Login</h1>
+  </section>
+)
+
+export default login
