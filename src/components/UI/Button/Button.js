b0c543d27109c36075fe37783d4bef452e478ffa
keenode <kstaffieri@gmail.com>
2018-04-05 14:39:31 -0700

    adding login form submission handler

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,6 +5,7 @@ import styles from './Button.css'
 const button = (props) => (
     <button
         className={[styles.Button, styles[props.btnType]].join(' ')}
+        type={props.type}
         onClick={props.clicked}>{props.children}</button>
 )
 
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Input from '../../components/UI/Controls/Input/Input'
+import Button from '../../components/UI/Button/Button'
 
 import styles from './Login.css'
 
@@ -47,6 +48,15 @@ class Login extends Component {
     this.setState({ loginForm: updatedLoginForm })
   }
 
+  loginHandler = (event) => {
+    event.preventDefault()
+    const formData = {}
+    for (let formElementId in this.state.loginForm) {
+      formData[formElementId] = this.state.loginForm[formElementId].value
+    }
+    console.log('[loginHandler] formData: ', formData)
+  }
+
   render() {
     const formElementsArray = []
     for (let key in this.state.loginForm) {
@@ -59,14 +69,17 @@ class Login extends Component {
       <section className={styles.Login}>
         <h1>Login</h1>
         <hr />
-        {formElementsArray.map(formElement => (
-          <Input 
-            key={formElement.id}
-            elementType={formElement.config.elementType} 
-            elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}
-            changed={(event) => { this.inputChangedHandler(event, formElement.id) }} />
-        ))}
+        <form onSubmit={this.loginHandler}>
+          {formElementsArray.map(formElement => (
+            <Input 
+              key={formElement.id}
+              elementType={formElement.config.elementType} 
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              changed={(event) => { this.inputChangedHandler(event, formElement.id) }} />
+          ))}
+          <Button type="submit">Login</Button>
+        </form>
       </section>
     )
   }
