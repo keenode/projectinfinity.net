25087bcdca41ca6f272e90f30afd831c81b93234
keenode <kstaffieri@gmail.com>
2018-04-07 23:04:10 -0700

    cleaning up login

--- server/passport/local/local-login.js
+++ server/passport/local/local-login.js
@@ -16,28 +16,27 @@ module.exports = new LocalStrategy({
       password: password.trim()
     }
   
-    // const newUser = new User(userData)
     User.findOne({ email: userData.email }).then(existingUser => {
       if (!existingUser) {
         console.log('invalid email or password!')
         return done(null, {
           error: 'Invalid email or password!'
         })
-      } else {
-        existingUser.comparePassword(userData.password, (passwordErr, isMatch) => {
-          if (!isMatch) {
-            return done(null, {
-              error: 'Invalid email or password!'
-            })
-          }
-    
-          jwt.sign({ user: existingUser }, config.jwtSecret, { expiresIn: '14 days' }, (err, token) => {
-            existingUser.token = token
-            console.log('logged in as user: ', existingUser)
-            return done(null, existingUser)
-          })
-        })
       }
+
+      existingUser.comparePassword(userData.password, (passwordErr, isMatch) => {
+        if (!isMatch) {
+          return done(null, {
+            error: 'Invalid email or password!'
+          })
+        }
+  
+        jwt.sign({ user: existingUser }, config.jwtSecret, { expiresIn: '14 days' }, (err, token) => {
+          existingUser.token = token
+          console.log('logged in as user: ', existingUser)
+          return done(null, existingUser)
+        })
+      })
     })
   }
 )
