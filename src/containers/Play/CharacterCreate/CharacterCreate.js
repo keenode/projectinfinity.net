d8a1646c12024e121d861267a08c6143f8a28781
keenode <kstaffieri@gmail.com>
2018-04-08 19:07:19 -0700

    adding makeshift form validation for create character screen

--- src/containers/Play/CharacterCreate/CharacterCreate.js
+++ src/containers/Play/CharacterCreate/CharacterCreate.js
@@ -9,46 +9,130 @@ import * as actions from '../../../store/actions'
 import styles from './CharacterCreate.css'
 
 class CharacterCreate extends Component {
+  state = {
+    createCharacterForm: {
+      name: {
+        elementType: 'input',
+        label: 'Character Name',
+        elementConfig: {
+          type: 'text',
+          name: 'name',
+          placeholder: 'Character Name'
+        },
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
+      },
+      gender: {
+        elementType: 'select',
+        label: 'Gender',
+        elementConfig: {
+          name: 'gender',
+          options: [
+            { value: 'male', displayValue: 'Male' },
+            { value: 'female', displayValue: 'Female' }
+          ]
+        },
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: true,
+        touched: false
+      },
+      race: {
+        elementType: 'select',
+        label: 'Race',
+        elementConfig: {
+          name: 'race',
+          options: [
+            { value: 'human', displayValue: 'Human' }
+          ]
+        },
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: true,
+        touched: false
+      },
+    },
+    formIsValid: false
+  }
+
+  checkValidation(value, rules) {
+    let isValid = true
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    return isValid
+  }
+
+  inputChangedHandler = (event, inputId) => {
+    const updatedForm = {
+      ...this.state.createCharacterForm
+    }
+    const updatedFormElement = {
+      ...updatedForm[inputId]
+    }
+    updatedFormElement.value = event.target.value
+    updatedFormElement.valid = this.checkValidation(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.touched = true
+    updatedForm[inputId] = updatedFormElement
+
+    let formIsValid = true
+    for (let inputId in updatedForm) {
+      formIsValid = updatedForm[inputId].valid && formIsValid
+    }
+
+    this.setState({ createCharacterForm: updatedForm, formIsValid: formIsValid })
+  }
+
   backHandler = () => {
     this.props.onPlayModeChanged('CharacterSelect')
   }
 
-  startHandler = () => {
+  createCharacterHandler = (event) => {
+    event.preventDefault()
     console.log('[createCharacterHandler]')
+    // this.props.onCreateCharacter(formData.name, formData.gender, formData.race)
   }
 
   render() {
+    const formElementsArray = []
+    for (let key in this.state.createCharacterForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.createCharacterForm[key]
+      })
+    }
     return (
       <div className={styles.CharacterCreate}>
         <h3>Character Create</h3>
         <Button clicked={this.backHandler}>Back</Button>
         <hr />
-        <Input 
-          id="input-character-name"
-          elementType="input"
-          label="Character Name"
-          elementConfig={{}} />
-        <Input 
-          id="input-gender"
-          elementType="select"
-          label="Gender"
-          elementConfig={{
-            options: [
-              { value: 'male', displayValue: 'Male' },
-              { value: 'female', displayValue: 'Female' }
-            ]
-          }} />
-          <Input 
-            id="input-race"
-            elementType="select"
-            label="Race"
-            elementConfig={{
-              options: [
-                { value: 'human', displayValue: 'Human' }
-              ]
-            }} />
-        <hr />
-        <Button clicked={this.startHandler}>Start</Button>
+        <form onSubmit={this.createCharacterHandler}>
+          {formElementsArray.map(formElement => (
+            <Input 
+              key={formElement.id}
+              id={'input-' + formElement.id}
+              elementType={formElement.config.elementType}
+              label={formElement.config.label}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
+              touched={formElement.config.touched}
+              changed={(event) => { this.inputChangedHandler(event, formElement.id) }} />
+          ))}
+          <hr />
+          <Button type="submit" disabled={!this.state.formIsValid}>Start</Button>
+        </form>
       </div>
     )
   }
