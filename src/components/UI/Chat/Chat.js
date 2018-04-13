43695a1bca2a96160f94c582b163ca24c59c9a1c
keenode <kstaffieri@gmail.com>
2018-04-12 20:59:52 -0700

    styling up chat box a bit nicer

--- src/components/UI/Chat/Chat.js
+++ src/components/UI/Chat/Chat.js
@@ -4,7 +4,16 @@ import styles from './Chat.css'
 
 const chat = props => (
   <div className={styles.Chat}>
-    <p>{props.messages}</p>
+    <ul className={styles.ChatRoomTabs}>
+      <li className={styles.ChatRoomTab}>System</li>
+      <li className={[styles.ChatRoomTab, styles.ChatRoomTabActive].join(' ')}>General</li>
+      <li className={styles.ChatRoomTab}>Region</li>
+    </ul>
+    <div className={styles.MessagesContainer}>
+      <p className={styles.Message}>
+        <span className={styles.MessageTimestamp}>[8:50pm]:</span> {props.messages}
+      </p>
+    </div>
   </div>
 )
 
