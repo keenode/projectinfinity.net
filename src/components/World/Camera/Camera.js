e85ffc56ae5ec9db277054738bb6aadf5f73e4a4
keenode <kstaffieri@gmail.com>
2018-04-13 17:57:05 -0700

    smooth keyboard controls, yay!

--- src/components/World/Camera/Camera.js
+++ src/components/World/Camera/Camera.js
@@ -2,9 +2,9 @@ class Camera {
   scene = null
   vx = 0
   vy = 0
-  accel = 20.0
-  maxVel = 20.0
-  deccelFactor = 0.95
+  accel = 2.0
+  maxVel = 10.0
+  friction = 0.25
   moving = {
     up: false,
     down: false,
@@ -14,66 +14,73 @@ class Camera {
 
   constructor() {
     console.log('[Camera] Constructed')
-    // document.onkeydown = document.onkeyup = this.handleKeyEvents.bind(this)
     document.onkeydown = this.handleKeyDown.bind(this)
     document.onkeyup = this.handleKeyUp.bind(this)
   }
 
   handleKeyDown(e) {
-    // console.log(e.which)
     if (e.which === 87 || e.which === 38) {
       console.log('up')
       this.moving.up = true
     }
-    if (e.which === 83 || e.which === 40) {
+    else if (e.which === 83 || e.which === 40) {
       console.log('down')
       this.moving.down = true
     }
-    if (e.which === 65 || e.which === 37) {
+    else if (e.which === 65 || e.which === 37) {
       console.log('left')
       this.moving.left = true
     }
-    if (e.which === 68 || e.which === 39) {
+    else if (e.which === 68 || e.which === 39) {
       console.log('right')
       this.moving.right = true
     }
-    this.move()    
   }
 
   handleKeyUp(e) {
-    // console.log(e.which)
     if (e.which === 87 || e.which === 38) {
       this.moving.up = false
     }
-    if (e.which === 83 || e.which === 40) {
+    else if (e.which === 83 || e.which === 40) {
       this.moving.down = false
     }
-    if (e.which === 65 || e.which === 37) {
+    else if (e.which === 65 || e.which === 37) {
       this.moving.left = false
     }
-    if (e.which === 68 || e.which === 39) {
+    else if (e.which === 68 || e.which === 39) {
       this.moving.right = false
     }
   }
 
-  move() {
+  update(delta) {
+    // Handle acceleration direction
     if (this.moving.up) {
       this.vy += this.accel
-    } else if (this.moving.down) {
+    }
+    if (this.moving.down) {
       this.vy -= this.accel
-    } else if (this.moving.left) {
+    }
+    if (this.moving.left) {
       this.vx += this.accel
-    } else if (this.moving.right) {
+    }
+    if (this.moving.right) {
       this.vx -= this.accel
     }
-  }
 
-  update(delta) {
-    const dirFactor = this.moving.up || this.moving.left ? 1 : -1
-    this.vx *= this.deccelFactor
-    this.vy *= this.deccelFactor
-    this.vx = Math.abs(this.vx) > this.maxVel ? this.maxVel * dirFactor : this.vx
-    this.vy = Math.abs(this.vy) > this.maxVel ? this.maxVel * dirFactor : this.vy
+    // Handle friction
+    this.vx -= this.friction * Math.sign(this.vx)
+    this.vy -= this.friction * Math.sign(this.vy)
+
+    // Handle max velocity 
+    const dirFactorX = this.moving.left ? 1 : -1
+    const dirFactorY = this.moving.up ? 1 : -1
+    this.vx = Math.abs(this.vx) > this.maxVel ? this.maxVel * dirFactorX : this.vx
+    this.vy = Math.abs(this.vy) > this.maxVel ? this.maxVel * dirFactorY : this.vy
+
+    console.log('vy: ' + this.vy)
+    console.log('vx: ' + this.vx)
+
+    // Set position
     this.scene.x += this.vx * delta
     this.scene.y += this.vy * delta
   }
