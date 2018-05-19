aa7488a8ab25a65b01212601b6edfabb07dbbd35
keenode <kstaffieri@gmail.com>
2018-05-19 15:17:14 -0700

    adding send chat message logic to redux store

--- src/store/actions/chat.js
+++ src/store/actions/chat.js
@@ -35,3 +35,38 @@ export const getChatMessagesError = () => {
     type: actionTypes.GET_CHAT_MESSAGES_ERROR
   }
 }
+
+/*
+ * SEND_CHAT_MESSAGE
+ */
+export const sendChatMessages = message => {
+  return dispatch => {
+    dispatch(sendChatMessagesStart())
+    axios.post('/api/chat/messages', message)
+      .then(res => {
+        dispatch(sendChatMessagesSuccess(res.data.messages))
+      })
+      .catch(err => {
+        dispatch(sendChatMessagesError())
+      })
+  }
+}
+
+export const sendChatMessagesStart = () => {
+  return {
+    type: actionTypes.SEND_CHAT_MESSAGE_START
+  }
+}
+
+export const sendChatMessagesSuccess = messages => {
+  return {
+    type: actionTypes.SEND_CHAT_MESSAGE_SUCCESS,
+    messages
+  }
+}
+
+export const sendChatMessagesError = () => {
+  return {
+    type: actionTypes.SEND_CHAT_MESSAGE_ERROR
+  }
+}
--- src/store/reducers/chat.js
+++ src/store/reducers/chat.js
@@ -3,25 +3,44 @@ import { updateObject } from '../../shared/utility'
 
 const initialState = {
   messages: [],
-  loadingMessages: false
+  loadingMessages: false,
+  sendingMessage: false
 }
 
 /*
  * Get Chat Messages
  */
-const geChatMessagesStart = (state, action) => {
+const getChatMessagesStart = (state, action) => {
   return updateObject(state, { loadingMessages: true })
 }
 
-const geChatMessagesSuccess = (state, action) => {
+const getChatMessagesSuccess = (state, action) => {
   return updateObject(state, {
     messages: action.messages,
     loadingMessages: false
   })
 }
 
-const geChatMessagesError = (state, action) => {
+const getChatMessagesError = (state, action) => {
   return updateObject(state, { loadingMessages: false })
+}
+
+/*
+ * Send Chat Message
+ */
+const sendChatMessageStart = (state, action) => {
+  return updateObject(state, { sendingMessage: true })
+}
+
+const sendChatMessageSuccess = (state, action) => {
+  return updateObject(state, {
+    messages: action.messages,
+    sendingMessage: false
+  })
+}
+
+const sendChatMessageError = (state, action) => {
+  return updateObject(state, { sendingMessage: false })
 }
 
 /*
@@ -29,9 +48,12 @@ const geChatMessagesError = (state, action) => {
  */
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.GET_CHAT_MESSAGES_START: return geChatMessagesStart(state, action)
-    case actionTypes.GET_CHAT_MESSAGES_SUCCESS: return geChatMessagesSuccess(state, action)
-    case actionTypes.GET_CHAT_MESSAGES_ERROR: return geChatMessagesError(state, action)
+    case actionTypes.GET_CHAT_MESSAGES_START: return getChatMessagesStart(state, action)
+    case actionTypes.GET_CHAT_MESSAGES_SUCCESS: return getChatMessagesSuccess(state, action)
+    case actionTypes.GET_CHAT_MESSAGES_ERROR: return getChatMessagesError(state, action)
+    case actionTypes.SEND_CHAT_MESSAGE_START: return sendChatMessageStart(state, action)
+    case actionTypes.SEND_CHAT_MESSAGE_SUCCESS: return sendChatMessageSuccess(state, action)
+    case actionTypes.SEND_CHAT_MESSAGE_ERROR: return sendChatMessageError(state, action)
     default:
       return state
   }
